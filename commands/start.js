750c6409d819ffba3c50757e927f170e5fcbad77
Alex Korzhikov <Alexander.Korzhikov@ing.nl>
2019-04-05 17:40:43 +0200

    Initial Commit

---
+++ commands/start.js
@@ -0,0 +1,33 @@
+const inquirer = require('inquirer')
+
+const configure = require('./configure')
+const { read, save } = configure
+let { questions, state: answered } = read()
+
+questions = questions
+  .map(({ question, options }, i) => ({ 
+    type: 'list',
+    message: question,
+    choices: options,
+    when(state){
+      if (answered.hasOwnProperty(i)) {
+        return false
+      }
+      
+      save(state)
+      return true
+    },
+    name: `${i}`
+  }))
+
+function start() {
+  inquirer.prompt(questions)
+    .then((result) => {
+      save(result)
+      console.log('Thanks for answering!')
+    })
+}
+
+module.exports = { 
+  start,
+}
